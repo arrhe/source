edead29dd32f6607069a7f7679ee168427fbc915
arrhe <haribj.05@gmail.com>
2015-01-29 11:07:30 +0530

    first commit

---
+++ chaton/src/wsapp/WsChat.java
@@ -0,0 +1,40 @@
+package wsapp;
+ 
+import java.io.IOException;
+import java.util.ArrayList;
+ 
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+ 
+@ServerEndpoint(value = "/wschat")
+public class WsChat{
+    //notice:not thread-safe
+    private static ArrayList<Session> sessionList = new ArrayList<Session>();
+    
+    @OnOpen
+    public void onOpen(Session session){
+        try{
+            sessionList.add(session);
+            //asynchronous communication
+            session.getBasicRemote().sendText("Hello!");
+        }catch(IOException e){}
+    }
+    
+    @OnClose
+    public void onClose(Session session){
+        sessionList.remove(session);
+    }
+    
+    @OnMessage
+    public void onMessage(String msg){
+        try{
+            for(Session session : sessionList){
+                //asynchronous communication
+                session.getBasicRemote().sendText(msg);
+            }
+        }catch(IOException e){}
+    }
+}
